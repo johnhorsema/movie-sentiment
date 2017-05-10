66dc3fc0f20a4ed195f4b7615c1293a559c4ceab
johnhorsema <jcema@connect.ust.hk>
2017-05-11 00:29:49 +0800

    Use dictionary to store movie tweets.

--- server.py
+++ server.py
@@ -1,14 +1,23 @@
 from pycorenlp import StanfordCoreNLP
 nlp = StanfordCoreNLP('http://localhost:9000')
 
-tweets = [
-"haha lol owned dad!!!! u may think its nice that u get a fathers day once a year, but there is a son day every week",
-"I don't know, dude. Probably science & shit. Worst answer I ever gave in a job interview. :(",
-"Saw a coworker washing a banana in the sink and wondering what she did with it.",
-"It's so cold this morning I had to separate my dogs poop into two separate bags and use them as hand warmers.",
-"Every time I tie my shoe I feel like I'm giving a faraway sniper the go signal.",
-"While they're a lot of fun on Halloween, did you know most jack-o-lanterns end up at the pound? Please. Next year, carve a puppy."
-]
+collection = {
+    "Guardians of the Galaxy":
+        [
+            "haha lol owned dad!!!! u may think its nice that u get a fathers day once a year, but there is a son day every week",
+            "I don't know, dude. Probably science & shit. Worst answer I ever gave in a job interview. :(",
+            "Saw a coworker washing a banana in the sink and wondering what she did with it.",
+            "It's so cold this morning I had to separate my dogs poop into two separate bags and use them as hand warmers.",
+            "Every time I tie my shoe I feel like I'm giving a faraway sniper the go signal.",
+            "While they're a lot of fun on Halloween, did you know most jack-o-lanterns end up at the pound? Please. Next year, carve a puppy."
+        ],
+    "Star Wars":
+        [
+            "It was great!",
+            "Amazing CGI effects",
+            "Great actors! It's nice to see a female protagonist taking lead!"
+        ]
+};
 
 def processTweet(tweet):
     input = tweet
@@ -23,11 +32,11 @@ def processTweet(tweet):
     totalSentiment = 0
     for s in res["sentences"]:
         totalSentiment+=int(s["sentimentValue"])
-        print"%d: '%s': %s %s" % (
-            s["index"],
-            " ".join([t["word"] for t in s["tokens"]]),
-            s["sentimentValue"], s["sentiment"])
-    print "TotalSent:%d" % totalSentiment
+    #     print"%d: '%s': %s %s" % (
+    #         s["index"],
+    #         " ".join([t["word"] for t in s["tokens"]]),
+    #         s["sentimentValue"], s["sentiment"])
+    # print "TotalSent:%d" % totalSentiment
     return totalSentiment
 
 def getRatio(a):
@@ -36,10 +45,17 @@ def getRatio(a):
     for sentiment in a:
         sumNeg+=(sentiment==0 or sentiment==1)
         sumPos+=(sentiment==3 or sentiment==4)
+    if sumNeg==0:
+        return sumPos
     return sumPos/sumNeg
 
-sentimentArr = []
-for t in tweets:
-    sentimentArr.append(processTweet(t))
-print "Sentiment ratio for %d tweets: %f" % (len(tweets), getRatio(sentimentArr))
+
+for movie in collection:
+    sentimentArr = []
+    tweets = collection.get(movie)
+    for t in tweets:
+        sentimentArr.append(processTweet(t))
+    print movie
+    print "Sentiment ratio for %d tweets: %f" % (len(tweets), getRatio(sentimentArr))
+    print("---")
 
